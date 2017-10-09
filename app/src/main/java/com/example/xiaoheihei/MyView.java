6b77435a40c9675d695f38842cc8d116532e2d2c
xhh_git <18734184215@163.com>
2017-10-09 11:23:10 +0800

    添加自定义view的属性设置

---
+++ app/src/main/java/com/example/xiaoheihei/MyView.java
@@ -0,0 +1,73 @@
+package com.example.xiaoheihei;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by xhh on 2017/10/9.
+ */
+
+public class MyView extends View {
+    /**
+     * 需要绘制的文字
+     */
+    private String mText;
+    /**
+     * 文本的颜色
+     */
+    private int mTextColor;
+    /**
+     * 文本的大小
+     */
+    private int mTextSize;
+    /**
+     * 绘制时控制文本绘制的范围
+     */
+    private Rect mBound;
+    private Paint mPaint;
+
+    public MyView(Context context) {
+        this(context, null);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray array = context.getTheme().obtainStyledAttributes(attrs,R.styleable.MyTextView,defStyleAttr,0);
+
+        mText =  array.getString(R.styleable.MyTextView_mytext);
+        mTextColor = array.getColor(R.styleable.MyTextView_textColor,Color.BLACK);
+        mTextSize = (int) array.getDimension(R.styleable.MyTextView_textSize,100);
+        array.recycle();
+        mPaint = new Paint();
+        mPaint.setTextSize(mTextSize);
+        mPaint.setColor(mTextColor);
+        //获得绘制文本的宽和高
+        mBound = new Rect();
+        mPaint.getTextBounds(mText, 0, mText.length(), mBound);
+    }
+
+
+    //API21
+//    public MyTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//        super(context, attrs, defStyleAttr, defStyleRes);
+//        init();
+//    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //绘制文字
+        canvas.drawText(mText, getWidth() / 2 - mBound.width() / 2, getHeight() / 2 + mBound.height() / 2, mPaint);
+    }
+}
+
